8c7831aca1077e02efc59a2933df327b014cce6d
mkevin <mkevin4596@gmail.com>
2023-06-16 15:00:22 -0300

    decoradores

--- seccion_3/01-typescript-intro/src/main.ts
+++ seccion_3/01-typescript-intro/src/main.ts
@@ -6,7 +6,9 @@ import './style.css'
 // import './topics/05-basic-destructuring'
 // import './topics/06-function-destructuring'
 // import './topics/07-import-exports'
-import './topics/08-classes'
+// import './topics/08-classes'
+// import './topics/09-generics'
+import './topics/10-decorators'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
 Hola Mundo
