a3c8b543968230ad1a682b94f6e04b49fdb0c331
mkevin <mkevin4596@gmail.com>
2023-07-05 07:41:21 -0300

    fin seccion 5

--- 03-gifs-app/src/app/gifs/services/gifs.service.ts
+++ 03-gifs-app/src/app/gifs/services/gifs.service.ts
@@ -12,7 +12,10 @@ export class GifsService {
   private _tagsHistory: string[] = []
   private apiKey:       string = '29z0EuRd0l6iXi0M5HlgmmqMZuDnOHNx'
   private serviceUrl:   string ='http://api.giphy.com/v1/gifs'
-  constructor( private http: HttpClient ) { }
+  constructor( private http: HttpClient ) {
+      this.loadLocalStorage();
+      console.log('Gifs Service Ready');
+  }
 
   get tagsHistory(){
     return [...this._tagsHistory];
@@ -27,6 +30,20 @@ export class GifsService {
 
     this._tagsHistory.unshift( tag );
     this._tagsHistory = this._tagsHistory.splice(0, 10)
+    this.saveLocalStorage();
+  }
+
+  private saveLocalStorage(): void {
+    localStorage.setItem('history', JSON.stringify(this._tagsHistory));
+  }
+
+  private loadLocalStorage(): void {
+    if (!localStorage.getItem('history')) return;
+
+    this._tagsHistory = JSON.parse( localStorage.getItem('history')! )
+
+    if( this._tagsHistory.length === 0) return;
+    this.searchTag( this._tagsHistory[0]);
   }
 
   searchTag( tag: string): void {
