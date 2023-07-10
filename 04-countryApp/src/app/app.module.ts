8eefa90a371adca843f69091884a6c0adb9d9e0b
mkevin <mkevin4596@gmail.com>
2023-07-10 13:20:39 -0300

    funcionamiento del search box

--- 04-countryApp/src/app/app.module.ts
+++ 04-countryApp/src/app/app.module.ts
@@ -3,7 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
-import { AppRoutingModule } from 'src/app-routing.module';
+import { AppRoutingModule } from './app-routing.module';
+import { CountriesModule } from './countries/countries.module';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { AppRoutingModule } from 'src/app-routing.module';
   imports: [
     AppRoutingModule,
     BrowserModule,
-    SharedModule
+    SharedModule,
+    CountriesModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
